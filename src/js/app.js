aad351365c6332eff1c5eebcdd2667803cc40f5b
Nikola Orlovski <nikikiko1@abv.bg>
2022-12-17 01:39:55 +0200

    Edit

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,10 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
-
+  const elements= document.getElementsByClassName("product")
+  
+  for (const e of elements) {
+    let price = e.querySelector(".price").innerHTML
+    e.setAttribute("data-price",price)
+  }
 });
